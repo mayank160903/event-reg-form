1ef23e8c8b49f8f01c29bc50933ad66158b280fe
mayank160903 <97187052+mayank160903@users.noreply.github.com>
2024-06-22 17:34:03 +0530

    first commit

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <div className="text-center p-6 bg-black text-4xl font-sans font-bold text-white flex flex-row flex-wrap">
       <div className='basis-1/2'>
-      Level-1 : Advanced Feedback Form
+      Level-1 : Event Registration Form
     </div>
     <div className='basis-1/2 text-blue-200'>
       <a href="https://github.com/mayank160903/event-reg-form">
