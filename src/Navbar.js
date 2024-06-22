8adf7faf29998fd260f8af477686fefe67e6ad14
mayank160903 <97187052+mayank160903@users.noreply.github.com>
2024-06-22 17:06:40 +0530

    final

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,9 +2,17 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='text-center p-6 bg-black text-4xl font-sans font-bold text-white'>
-      Level-1 : Event Registration Form
+    <div className="text-center p-6 bg-black text-4xl font-sans font-bold text-white flex flex-row flex-wrap">
+      <div className='basis-1/2'>
+      Level-1 : Advanced Feedback Form
     </div>
+    <div className='basis-1/2 text-blue-200'>
+      <a href="https://github.com/mayank160903/event-reg-form">
+        View Source Code
+      </a>
+    </div>
+    </div>
+    
   )
 }
 
